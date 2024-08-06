eee3ed7f0910501dc777052ffe19f821b84434ba
Marcel Almeida <marcel_amd@hotmail.com>
2024-08-06 09:19:24 -0300

    Criado o teste1.py com os testes descritos no Item 2 das tasks.

--- tasks.md
+++ tasks.md
@@ -3,6 +3,8 @@
 * Verificar os textos de exemplo usando a dica de forma invertida: Exemplo1 com dica do 2 e vice versa.
 
 ## 2 - Analisar algum outro texto (sugestões)
+* teste1.py: No início do texto existe a palavra **time** destacada, criei o teste1.py para verificar se as palavras do início do texto até esta palavra fazem parte do dicionário. - NÃO FUNCIONA
+Outro teste foi pegar a frase: "The beauty of trithemian seeds is that they hide in plain sight. If you’ve read this far, you’ve read every word required to access a wallet with .03 BTC. Good luck!" e verificar quais palavras fazem parte do dicionário, mas apenas 10 fazem. - NÃO FUNCIONA
+
 
 ## 3 - Analisar as imagens
-
---
+++ teste1.py
@@ -0,0 +1,32 @@
+
+from bip32 import *
+
+palavras = []
+for i in open('palavras.txt').readlines():
+    palavras.append(i[:-1])
+
+
+
+def main():
+
+    texto1 = "the beauty of trithemian seeds is that they hide in plain sight if you ve read this far you ve read every word required to access a wallet with .03 BTC good luck"
+    texto1_in_palavras = ''
+
+    texto1 = texto1.split()
+    print(len(texto1))
+
+    for i in texto1:
+        if i in palavras:
+            #print("Tem: ", i)
+            texto1_in_palavras = texto1_in_palavras + " " + i
+
+    texto1_in_palavras = texto1_in_palavras[1:]
+    print(texto1_in_palavras)
+    gera_dados_carteira(texto1_in_palavras)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
