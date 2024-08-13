300ef19754c3c6f0a0e0ea69f2344f0e270a7715
Marcel Almeida <marcel_amd@hotmail.com>
2024-08-13 09:39:00 -0300

    Agora sim adicionado o testes2 para fazer a busca das palavras fazendo permutação entre elas.

--- bip32.py
+++ bip32.py
@@ -6,50 +6,42 @@ from bitcoinlib.wallets import Wallet
 from bitcoinlib.keys import Signature
 import bitcoinlib
 
-def gera_dados_carteira(words):
+
+def gera_mnemonic(word):
+    words = Mnemonic().to_mnemonic(word)
+    print(words)
+
+def gera_dados_carteira(words, passwd):
 
     try:
-
-        #Detect Language
-        #ret = Mnemonic().detect_language('faint lonely scale gate camera shoulder adult game medal language payment')
-
-        #words = 'chunk gun celery million wood kite tackle twenty story episode raccoon dutch'
-        #words = Mnemonic().generate(strength=128)
-        #print(words)
         entropy = Mnemonic().to_entropy(words)
-        #print('Entropy: ', entropy)
-        #print(entropy.hex())
-        seed = Mnemonic().to_seed(words)
-        #print('Seed: ', seed)
+        seed = Mnemonic().to_seed(words, password=passwd)
         #print(seed.hex())
-
         hd_key = HDKey.from_seed(seed)
-        #print("HDKey: ", hd_key)
-    
-    
-  
-
-        paths = ["m/0/0", "m/44'/0'/0'/0/0", "m/49'/0'/0'/0/0", "m/84'/0'/0'/0/0"]
-
-
-        for i in paths:
-            key = hd_key.subkey_for_path(i)
-            #print(type(key)) 
-            #print(key.info())
-            #print(key.as_json())
-            print(i)
-            print("ADRRESS: ", key.address())
-            print("ADRRESS: ", key.address(script_type='p2pkh'))
-            print("ADRRESS: ", key.address(script_type='p2pkh', encoding='bech32'))
-            print("ADRRESS: ", key.address(script_type='pp2sh'))
-            print("ADRRESS: ", key.address(script_type='pp2sh', encoding='bech32'))
-            print("PUBLIC_KEY: ", key.public())
-            print("WIF: ",key.wif_key())
-            print("\n")
-    
     except Exception as e:
-        print("Error Code: ", e)
+        #print("Error Code: ", e)
         return False
     
-    return True
+    
+    #paths = ["m/0/0", "m/44'/0'/0'/0/0", "m/49'/0'/0'/0/0", "m/84'/0'/0'/0/0"]
+    paths = ["m/0/0", "m/44'/0'/0'/0/0", "m/49'/0'/0'/0", "m/84'/0'/0'/0/0"]
+    info = []
+
+    for i in range(len(paths)):
+        key = hd_key.subkey_for_path(paths[i])
+        #print(type(key)) 
+        #print(key.info())
+        #print(key.as_json())
+        #print(i)
+        #print("ADRRESS: ", key.address())
+        #print("ADRRESS: ", key.address(script_type='pp2sh', encoding='bech32'))
+        #print("PUBLIC_KEY: ", key.public())
+        #print("WIF: ",key.wif_key())
+        #print("\n")
+        info.append(key.address())
+        info.append(key.address(script_type='pp2sh', encoding='bech32'))
+        info.append(key.wif_key())
+
+    
+    return info
 
--- examples.py
+++ examples.py
@@ -20,8 +20,9 @@ def words_in_positions(texto, positions):
 
 def main():
 
-    texto1 = "the faint lines across the lonely expanse make scale hard to determine as I walk towards the gate I sling my camera over my shoulder and prepare to enter the old palace The door pushes open easily and I walk into the courtyard no adult in sight I place my camera on the table and get to work searching through the waste As I dust off an ancient board game something shiny catches my eye I reach towards the cupboard and pick up a gold medal Upon closer inspection I notice an inscription that seems to resemble a foreign language I pocket the trinket with the hope that this might make for a good enough payment at the border Any valuable will do"
+    #texto1 = "the faint lines across the lonely expanse make scale hard to determine as I walk towards the gate I sling my camera over my shoulder and prepare to enter the old palace The door pushes open easily and I walk into the courtyard no adult in sight I place my camera on the table and get to work searching through the waste As I dust off an ancient board game something shiny catches my eye I reach towards the cupboard and pick up a gold medal Upon closer inspection I notice an inscription that seems to resemble a foreign language I pocket the trinket with the hope that this might make for a good enough payment at the border Any valuable will do"
     texto2 = "such an asset to be represented by an experienced and mature lawyer particularly when you have a trial in front of the supreme court of the USA load your argument with logic and do not provide ways to escape symbol of cultural diversity should be used as often as possible in order to support the story our client clearly did not make that complex and improvised bomb himself they were on their way to his friends picnic by the river when he noticed a suspicious person pretending to do aerobic exercises he immediately pointed it out to his friends their whereabouts arent a mystery during the attack they were ordering ginger tea with honey to bring to the picnic"
+    texto1 = "this method eliminates the need for a number pair heightening strength at the cost of complexity in this case a geographic landmark is chosen that relates to a plot built around the given seed phrases the GPS coordinates of this landmark dictate the placement of the seed phrases among the body of text to maintain relatively easy dispersal with slight variation every digit read increases by ten so 21.493021 translates to"
 
     texto1 = texto1.split()
     #print(texto1)
@@ -30,6 +31,7 @@ def main():
 
     posicoes1 = [2,6,9,18,22,25,45,70,86,100,113,116]
     posicoes2 = [3,18,28,39,40,56,67,77,80,90,104,114]
+    posicoes1 = [2, 11, 24, 39,43, 50, 62, 71]
 
     resp1 = []
     resp2 = []
